ae453b4fb585af5a1d76acc01a454c64dc71f17d
rahulg3520 <rahul@nestgroup.net>
2022-11-30 10:49:00 +0530

    add friends api

--- src/app/add-friends/add-friends.component.ts
+++ src/app/add-friends/add-friends.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-friends',
@@ -12,19 +13,44 @@ export class AddFriendsComponent {
   friendNickName=""
   DescribeYourFriend=""
 
+  constructor(private api:ApiService){}
+
   readValues = () =>
   {
 
     let data:any=
     {
-      "Name":this.Name,
-      "friendName":this.friendName,
-      "friendNickName":this.friendNickName,
-      "DescribeYourFriend":this.DescribeYourFriend
+      "Name":this.Name,"friendName":this.friendName,"friendNickName":this.friendNickName,"DescribeYourFriend":this.DescribeYourFriend
     
     }
 
     console.log(data);
+
+    this.api.addFriends(data).subscribe(
+
+      (response:any)=>
+      {
+        console.log(response)
+
+        if(response.status == "success")
+        {
+
+          alert("Friend data added successfully")
+          this.Name=""
+          this.friendName=""
+          this.friendNickName=""
+          this.DescribeYourFriend=""
+          
+        } 
+        else 
+        {
+          
+          alert("something went wrong")
+        }
+      }
+
+      
+    )
     
 
   }
